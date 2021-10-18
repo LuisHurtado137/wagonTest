f2efbe00ef0555f88d4be3960b0be32d968fccb9
Luis Hurtado <luisferhc352@gmail.com>
2021-10-18 18:12:51 -0500

    Creé el archivo Header con su logo y sus respectivas pestañas y le di estilos

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import "../Header/Header.css"
+import "../../App.css"
+import {Main} from '../Main Page/MainPage'
+import Logo from '../../assest/NameLogo.svg'
+import Elipse from '../../assest/Elipse 3.svg'
+
+export const Header = () =>{
+    return (
+        <div>
+           <header className="header">  
+           <img src={Elipse} alt="decoration" className="elipse" />
+                <ul className="header-items" >
+                    <li>PROYECTOS</li>
+                    <li>SKILLS</li>
+                    <img src={Logo} alt="logo"/>
+                    <li>PROYECTOS</li>
+                    <li>PROYECTOS</li>
+                </ul>
+                <Main/>
+            </header>
+            <section className="header2"/> 
+        </div>
+        
+    ) 
+}

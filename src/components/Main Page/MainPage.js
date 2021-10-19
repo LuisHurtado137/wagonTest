09495791e9e7991c0f3244a8c9b522ab9ee65a72
Luis Hurtado <luisferhc352@gmail.com>
2021-10-19 16:59:14 -0500

    agregué le título y párrafo de la página principal

---
+++ src/components/Main Page/MainPage.js
@@ -0,0 +1,13 @@
+import '../Main Page/MainPage.css'
+import "../../App.css"
+
+export const Main = () => {
+    return(
+        <div className="main-page" >
+            <h4 className="titles">Lorem Ipsum</h4>
+            <div className="main-paragraph" >
+                <span className="text-paragraph paragraphs">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</span>    
+            </div>
+        </div> 
+    )
+}
